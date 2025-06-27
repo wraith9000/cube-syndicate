874ac0c5e51355012cd19826351e8e210737708b
NeterNFT <netern.nft@gmail.com>
2025-06-28 00:47:16 +0300

    g

--- assets/js/script.js
+++ assets/js/script.js
@@ -450,28 +450,28 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Typing effect for hero title
-    function typeWriter(element, text, speed = 100) {
-        let i = 0;
-        element.innerHTML = '';
-        
-        function type() {
-            if (i < text.length) {
-                element.innerHTML += text.charAt(i);
-                i++;
-                setTimeout(type, speed);
-            }
-        }
-        type();
-    }
+    // function typeWriter(element, text, speed = 100) {
+    //     let i = 0;
+    //     element.innerHTML = '';
+    //     
+    //     function type() {
+    //         if (i < text.length) {
+    //             element.innerHTML += text.charAt(i);
+    //             i++;
+    //             setTimeout(type, speed);
+    //         }
+    //     }
+    //     type();
+    // }
 
     // Initialize typing effect when page loads
-    const heroTitle = document.querySelector('.hero-title');
-    if (heroTitle) {
-        const originalText = heroTitle.textContent;
-        setTimeout(() => {
-            typeWriter(heroTitle, originalText, 50);
-        }, 500);
-    }
+    // const heroTitle = document.querySelector('.hero-title');
+    // if (heroTitle) {
+    //     const originalText = heroTitle.textContent;
+    //     setTimeout(() => {
+    //         typeWriter(heroTitle, originalText, 50);
+    //     }, 500);
+    // }
 
     // Counter animation for features
     function animateCounters() {
